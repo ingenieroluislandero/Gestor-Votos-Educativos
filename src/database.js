5b4575da5c4b45283c3c8f275971f1be5068311c
ingenieroluislandero <luisg2396@gmail.com>
2019-01-08 20:53:13 -0500

    creamos la conexion a la base de datos en el archivo database.js

---
+++ src/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+
+mongoose.set('useFindAndModify', false)
+mongoose.connect('mongodb://localhost/gestorVotos', {
+	useCreateIndex: true,
+	useNewUrlParser:true
+})
+
+	.then(db => console.log('DB is connected'))
+	.catch(err => console.error(err))
