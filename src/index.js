504d0899dcf50f6541ca0347189bbfd4e96e67a9
ingenieroluislandero <luisg2396@gmail.com>
2019-01-08 20:22:14 -0500

    configurando index.js  settings

--- src/index.js
+++ src/index.js
@@ -8,12 +8,24 @@ const express = require('express'),
 	 app = express()
 
 //Initializations
-//settins
+require('./database')
+require('./config/passport')
+//settings
+app
+	.set('port', process.env.PORT || 3000)
+	.set('views', path.join(__dirname, 'views'))
+	.engine('.hbs', exphbs({
+  		defaultLayout: 'main',
+  		layoutsDir: path.join(app.get('views'), 'layouts'),
+  		partialsDir: path.join(app.get('views'), 'partials'),
+  		extname: '.hbs'
+		}))
+	.set('view engine', '.hbs')
 //middlewares
 //gloval variables
 //routes
 //static files
 //server is listening
 app.listen(app.get('port'), () => {
-  console.log('Server on port', app.get('port'));
+  console.log('Server on port', app.get('port'))
 });
